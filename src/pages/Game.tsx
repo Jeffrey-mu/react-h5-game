07460ba694bce3d9da963e29062c0efa5442c68c
Jeffrey <1115378579@qq.com>
2024-06-07 14:32:37 +0800

    wip: 区分环境

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -18,8 +18,8 @@ import { Label } from "@/components/ui/label"
 import { useToast } from "@/components/ui/use-toast"
 import { Toaster } from "@/components/ui/toaster"
 export const host = window.location.host
-// export  const GameSeriesPath = 'http://' + host
-export const GameSeriesPath = 'http://101.43.206.247:3230'
+export  const GameSeriesPath = 'http://' + host
+// export const GameSeriesPath = 'http://101.43.206.247:3230'
 function Game() {
   const [data, setData] = useState<groupByDateReturn[]>([])
   const [addGame, setAddGame] = useState<string>('')
